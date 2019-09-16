18e13e8ba017df56748532485024c0a293f967c7
GewoonGianni <giannimeesters@gmail.com>
2019-09-16 12:04:45 +0200

    first commit

---
+++ les_2/assets/app.js
@@ -0,0 +1,20 @@
+// console.log('hello')
+
+// var firstName = 'Gianni'
+// var lastName = 'Meesters'
+// var age = 17;
+
+// let message = `mijn naam is ${firstName} ${lastName} en ik ben ${age} jaar oud`
+// console.log(message);
+
+// var priceOfAnApple = 0.59;
+// var numberOfApples = 200;
+
+// var price = priceOfAnApple * numberOfApples
+
+// let message = `Als je ${numberOfApples} appels koopt kost dat ${price}`
+// console.log(message)
+
+var message = `Please visit the HZ univirsity of applied science`
+var replace = message.replace (`applied`, `mad`)
+console.log(replace);
