08898e55ab71233d658928a73ca35581aaec221a
GewoonGianni <giannimeesters@gmail.com>
2019-09-16 17:16:12 +0200

    added eindopdracht les 2

---
+++ les_2_eindopdracht/assets/app.js
@@ -0,0 +1,22 @@
+// 1 
+var number = 9
+var oddEven = number % 2;
+
+if (oddEven == 1) {
+    alert('The number is odd');
+} else {
+    alert('the number is even');
+}
+
+// 2
+var message = ('programming is not so cool');
+var replace = message.replace(` not `,` `);
+console.log(replace);
+
+// 3
+var number2 = 1400;
+var message2 = 'Ik woon in Naboo';
+
+console.log(number2 < message2);
+
+// ik vergelijk hier een getal met een zin, het zal altijd false zijn en er is geen reden om deze twee te vergelijken.
